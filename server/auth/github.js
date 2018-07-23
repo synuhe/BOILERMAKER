0d5e20cd2b9cc26c270f9e5ce419c2c1e828f139
Synuhe <synuhe.uribe@gmail.com>
2018-07-23 16:44:14 -0400

    changed some stuff with github

--- server/auth/github.js
+++ server/auth/github.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const router = require('express').Router()
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const GitHubStrategy = require('passport-github').Strategy;
 const {User} = require('../db/models')
 module.exports = router
 
@@ -18,24 +19,24 @@ module.exports = router
  * process.env.GOOGLE_CALLBACK = '/your/google/callback'
  */
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+  console.log('Github client ID / secret not found. Skipping Github OAuth.')
 } else {
-  const googleConfig = {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+  const githubConfig = {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK
   }
 
-  const strategy = new GoogleStrategy(
-    googleConfig,
+  const strategy = new GitHubStrategy(
+    githubConfig,
     (token, refreshToken, profile, done) => {
-      const googleId = profile.id
+      const githubId = profile.id
       const name = profile.displayName
-      const email = profile.emails[0].value
+      const email = profile.username+"@email.com" //profile.emails[0].value
 
       User.findOrCreate({
-        where: {googleId},
+        where: {githubId},
         defaults: {name, email}
       })
         .then(([user]) => done(null, user))
@@ -45,11 +46,11 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
   passport.use(strategy)
 
-  router.get('/', passport.authenticate('google', {scope: 'email'}))
+  router.get('/', passport.authenticate('github', {scope: 'email'}))
 
   router.get(
     '/callback',
-    passport.authenticate('google', {
+    passport.authenticate('github', {
       successRedirect: '/home',
       failureRedirect: '/login'
     })
